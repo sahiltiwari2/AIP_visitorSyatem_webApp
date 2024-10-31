b90f84b33c0e69054b8349d1bc07bc69b7bfca55
sahiltiwari2 <sahiltiwari2005@gmail.com>
2024-10-31 23:16:38 +0530

    Solved some errors

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import TopBar from '@/components/topBar'
 
 const form = () => {
   return (
-    <div>form</div>
+    <div>
+      <TopBar pageName='Appointment Form' />
+      <div className='pt-2'>Please fill out the form below to schedule an appointment. Ensure all fields are completed accurately to avoid any delays in
+        processing your request.
+      </div>
+    </div>
   )
 }
 
