845e6847cacdcaf9f72423c4c77e810a85a8259f
sahiltiwari2 <sahiltiwari2005@gmail.com>
2024-11-17 23:52:55 +0530

    Frontend for profoile done

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,6 +8,11 @@ import TopBar from '@/components/topBar'
 import settingIcon from '@/public/settingIcon.jpg'
 import { Icons } from 'react-toastify'
 import Image from 'next/image'
+import { Icon } from "@chakra-ui/react"
+import { IoMdSettings } from "react-icons/io";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { HiUserCircle } from "react-icons/hi2";
+import { RiLogoutCircleLine } from "react-icons/ri";
 
 const page = () => {
   const router = useRouter()
@@ -27,7 +32,8 @@ const page = () => {
         managing IT infrastructure and software development projects. Oliver is known for his attention to detail and his ability to solve
         complex problems efficiently. In his free time, he enjoys hiking and photography.
       </div>
-      <div className='flex justify-center mt-10 gap-3 text-2xl w-screen'>
+      <div className='flex justify-center mt-10  text-[150px] w-screen'><HiUserCircle /></div>
+      <div className='flex justify-center mt-5 gap-3 text-2xl w-screen'>
         <span className='font-semibold'>Current User:</span><span>Sahil Tiwari</span>
       </div>
       <div className='flex justify-center mt-4 gap-3 text-2xl w-screen'>
@@ -36,17 +42,27 @@ const page = () => {
       <div className='flex justify-center mt-4 gap-3 text-2xl w-screen'>
         <span className='font-semibold'>Email:</span><span>Sahiltiwari2005@gmail.com</span>
       </div>
-      <div className='font-bold text-xl m-10'>
-        Settings
-      </div>
-      <div>
-        <Button>
-          <Image src={settingIcon} alt='' height={100} width={100}/>
+      <div className='mt-10 w-screen p-3'>
+        <Button style={{ background: '#FFFFFF' }} className="w-full  text-xl h-11 hover:shadow-lg transition-all duration-700 delay-150" variant='bordered'>
+          <div className='w-full flex items-center text-xl'>
+            <div className='w-full flex items-center gap-3'>
+            <IoMdSettings />
+            Settings
+            </div>
+            <MdOutlineKeyboardArrowRight />
+          </div>
+        </Button>
+        <Button style={{ background: '#FFFFFF' }} className="w-full  text-xl h-11 hover:shadow-lg transition-all duration-700 delay-150 mt-3" variant='bordered'onClick={handelSignout}>
+          <div className='w-full flex items-center text-xl'>
+            <div className='w-full flex items-center gap-3'>
+            <RiLogoutCircleLine />
+            Log Out
+            </div>
+            <MdOutlineKeyboardArrowRight />
+          </div>
         </Button>
       </div>
-      <Button onClick={handelSignout}>
-        Log Out
-      </Button>
+      
     </div>
   )
 }
