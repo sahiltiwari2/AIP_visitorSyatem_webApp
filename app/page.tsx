3a103a01ed6495952360dbe7c9ab67c4cf35e46e
sahiltiwari2 <sahiltiwari2005@gmail.com>
2025-01-29 16:06:43 +0530

    Increased spacing between home vid and text

--- app/page.tsx
+++ app/page.tsx
@@ -26,15 +26,7 @@ export default function Home() {
         clients like TVS and Greaves, while beginning exports to Italy. The manufacturing facilities have been upgraded multiple times since
         1995 to meet international standards, and now nearly 50% of their output is exported to 35 countries.
       </div>
-      <div className="w-full h-[250px]  flex items-center justify-center text-3xl font-extrabold mt-12 mb-12">
-
-        {/* <iframe 
-    width="500" 
-    height="250" 
-    src="https://www.youtube.com/embed/Ca_85iPEzNM?autoplay=1&mute=1&controls=0&fs=0&modestbranding=1&rel=0" 
-    title="Abilities India Pistons & Rings Ltd 1" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share">
-</iframe> */}
+      <div className="w-full h-[350px] flex items-center justify-center text-3xl font-extrabold mt-12 mb-12">
         <div
           style={{
             position: 'relative',
@@ -55,7 +47,7 @@ export default function Home() {
               left: '50%',
               transform: 'translate(-50%, -50%)',
               width: '110%', // Slightly stretch the width
-              height: '110%', // Slightly stretch the height
+              height: '100%', // Slightly stretch the height
               objectFit: 'cover', // Make the video fill the container while allowing minor stretching
               pointerEvents: 'none', // Disable user interaction
             }}
